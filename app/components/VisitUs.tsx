37b81411421b541f1039f13e944ee7eb9856d92a
Megan Byers <megan@designelixir.studio>
2025-12-29 23:35:29 -0700

    add email fonts

--- app/components/VisitUs.tsx
+++ app/components/VisitUs.tsx
@@ -17,11 +17,13 @@ export default function VisitUs() {
           <Image src="/flora-3.svg" alt="floral detail" height={25} width={25} style={{marginLeft: '25px'}}></Image>
       </div>
       <h3 className="centered-text cream-text">
-        You can now come for a visit! Come and shop our cheeses at the <Link href="https://share.google/cWO3BJQooefN6bzhX">Jumpin' Good Country Store</Link>, also offering local salami, crackers, goat fudge, gifts, horse tack, and more. Meet our young goats (hopefully future milk producers!) who were born spring of 2025 and are eager to meet you.
+        Come for a visit! Shop our cheeses at the <Link href="https://www.google.com/maps/place/Jumpin%E2%80%99+Good+Country+Store+%26+Buck+n%E2%80%99+Bloom+Cheese+Shop/data=!4m2!3m1!1s0x0:0xfa6afe646a042e2a?sa=X&ved=1t:2428&ictx=111" target="_blank">Jumpin Good Country Store</Link>, also offering salami, crackers, farm fresh eggs, and goat treats. Say hi to our young goats and hens! 
+        &nbsp;<Link href="/tour">Better yet, take a tour of the farm to learn all about our creamery and animals!</Link>
+
       </h3>
       <div className="flex-center-center basic-padding visit-us-button-wrapper">
-          <Link target="_blank" style={{marginRight: '10px'}} href="https://www.google.com/maps/place/Jumpin%E2%80%99+Good+Country+Store+%26+Buck+n%E2%80%99+Bloom+Cheese+Shop/data=!4m2!3m1!1s0x0:0xfa6afe646a042e2a?sa=X&ved=1t:2428&ictx=111">
-          <button>Jumpin' Good Country Store Hours</button>
+          <Link target="_blank" style={{marginRight: '10px'}} href="/tour">
+          <button>Take a Tour</button>
           </Link>
           <Link target="_blank" href="https://www.google.com/maps?client=firefox-b-1-d&sca_esv=a1588e28351376e1&lsig=AB86z5VgdV8ohF0-ZZTYCWzNpas2&biw=981&bih=1012&dpr=2&um=1&ie=UTF-8&fb=1&gl=us&sa=X&geocode=KRdNUSd9tWqHMTQUGxTLQ8bC&daddr=31700+US-24,+Buena+Vista,+CO+81211">
             <button>Directions</button>
--- app/tour/page.tsx
+++ app/tour/page.tsx
@@ -3,9 +3,9 @@
 export default function Home() {
   return (
     <main className="flex-center-center flex-column fade-in" id="tour">
-      <div className="flex-center-center flex-column basic-padding" style={{marginTop: '150px'}}>
+      <div className="flex-center-center flex-column basic-padding" style={{marginTop: '250px'}}>
       <h2>Tour The Farm</h2>
-      <h3>Coming Soon!</h3>
+      <h3 className="centered-text">Learn about artisan cheesemaking in the Colorado Rockies! Together we will visit the milking parlor, peek inside the creamery, learn how cheese is made, go deep underground into the cheese cave, play with the young goats, greet the hens, and taste some cheese made right here on the farm. A wonderful way to learn and explore for the whole family! </h3>
       </div>
     </main>
   );
