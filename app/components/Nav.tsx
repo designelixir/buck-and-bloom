6e79010056d4bc33ee54aeff27971f4a2a977175
Megan Byers <megan@designelixir.studio>
2025-12-30 00:45:10 -0700

    add tour page

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -92,9 +92,10 @@ export default function Nav() {
 
       {open && (
         <div className="mobile-menu flex-center-center flex-column mobile-show">
-          <a id="shopLink" onClick={handleLinkClick} href="/#shop" className="nav-link shop-link">Shop</a>
+          <a id="shopLink" onClick={handleLinkClick} href="/shop/local-cheese-share" className="nav-link shop-link">Cheese Share</a>
           <a id="marketsLink" onClick={handleLinkClick} href="/#markets" className="nav-link markets-link">Markets</a>
           <a id="visitLink" onClick={handleLinkClick} href="/#visit-us" className="nav-link visit-us-link">Visit Us</a>
+          <a id="tourLink" onClick={handleLinkClick} href="/tour" className="nav-link tour-link">Tour the Farm</a>
           <a id="aboutLink" onClick={handleLinkClick} href="/#about" className="nav-link about-link">About</a>
           <a id="contactLink" onClick={handleLinkClick} href="/#contact" className="nav-link contact-link">Contact</a>
           
